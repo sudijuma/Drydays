e0411f0e3e68d9c74b4966f0caf885622e0fce44
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-04-24 17:55:03 +0200

    gettting the image
    and trying to link

--- script/index.js
+++ script/index.js
@@ -3,7 +3,10 @@ const queryString = window.location.search;
 
 const params = new URLSearchParams(queryString);
 console.log(params.get("id"));
-const API_URL = "https://mwakie.flywheelsites.com/wp-json/wc/store/products";
+const productID = params.get("id");
+
+const API_URL = `https://mwakie.flywheelsites.com/wp-json/wc/store/products/`;
+console.log(API_URL);
 
 const productDisplay = document.querySelector(".featured-products")
 
@@ -13,11 +16,11 @@ async function displayJackets() {
         const productResponse = await productCall.json();
         for (let i = 0; i < productResponse.length; i++) {
             productDisplay.innerHTML += `      <div class="featured-products-item">
-            <div class="featured-products-item-image"><img src=""></div>
-            <p class="title"></p>
-            <p class="description">${productResponse[i].name}</p>
-            <p class="price">${productResponse[i].prices.price}</p>
-            <button>view item</button>
+            <div class="featured-products-item-image"><img src="${productResponse[i].images[0].src}"></div>
+            <p class="title">${productResponse[i].name}</p>
+            <p class="short-description"></p>
+            <p class="price">${productResponse[i].prices.price}kr</p>
+            <button a href="specific.html?name=${productResponse[i].name}">view item</button>
           </div>`
         }
     } catch (e) {
