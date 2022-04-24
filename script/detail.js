896ee5c85b68b661fa8a14d3bf5c7dc16838d6db
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-04-25 00:27:11 +0200

    s

--- script/detail.js
+++ script/detail.js
@@ -5,25 +5,24 @@ console.log(params.get("id"));
 const productID = params.get("id");
 
 const API_URL = `https://mwakie.flywheelsites.com/wp-json/wc/store/products?${productID}`;
-console.log(API_URL);
 
 const productDisplay = document.querySelector(".featured-products");
 
-async function displayJackets() {
+async function displayDetail() {
   try {
     const productCall = await fetch(API_URL);
     const productResponse = await productCall.json();
     for (let i = 0; i < productResponse.length; i++) {
-      productDisplay.innerHTML += `      <div class="featured-products-item">
-            <div class="featured-products-item-image"><img src="${productResponse[i].images[0].src}" ></div>
-            <p class="title"></p>
-            <p class="short-description"></p>
-            <p class="price">${productResponse[i].prices.price}kr</p>
-            <button onclick="location.href='/specific.html?=${productResponse[i].id}'">view item</button>
-          </div>`;
+      productDisplay.innerHTML = `      <div class="featured-products-item">
+      <div class="featured-products-item-image"><img src="${productResponse[i].images[0].src}" ></div>
+      <p class="title">${productResponse[i].name}</p>
+      <p class="short-description">${productResponse[i].description}</p>
+      <p class="price">${productResponse[i].prices.price}kr</p>
+      <button onclick="location.href='/specific.html?=${productResponse[i].id}'">view item</button>
+    </div>`;
     }
   } catch (e) {
     console.log(e);
   }
 }
-displayJackets();
+displayDetail();
