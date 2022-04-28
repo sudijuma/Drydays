7ed645c358796b208d9e785fab8aa9f398e37b26
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-04-28 17:42:09 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,13 +1,4 @@
-/* console.log(document.location.search)
-const queryString = window.location.search;
-
-const params = new URLSearchParams(queryString);
-console.log(params.get("id"));
-const productID = params.get("name");
- */
 const API_URL = `https://mwakie.flywheelsites.com/wp-json/wc/store/products/`;
-console.log(API_URL);
-
 const productDisplay = document.querySelector(".featured-products");
 
 async function displayJackets() {
@@ -15,8 +6,8 @@ async function displayJackets() {
     const productCall = await fetch(API_URL);
     const productResponse = await productCall.json();
     for (let i = 0; i < productResponse.length; i++) {
-      productDisplay.innerHTML += `      <div class="featured-products-item">
-            <div class="featured-products-item-image"><img src="${productResponse[i].images[0].src}" ></div>
+      productDisplay.innerHTML += `<div class="featured-products-item">
+            <div class="featured-products-item-image"><img src="${productResponse[i].images[0].src}"></div>
             <p class="title"></p>
             <p class="short-description"></p>
             <p class="price">${productResponse[i].prices.price}kr</p>
